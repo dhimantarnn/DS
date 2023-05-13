eedd985a0001fab42c0414d239208dca85a729c8
tarn <tarndhiman9988@gmail.com>
2023-05-13 21:03:48 +0530

    init

---
+++ 01.cpp
@@ -0,0 +1,49 @@
+// WAP for removing duplicate values from an array
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a range: ";
+    cin>>n;
+
+
+    int arr[n],j=0;
+    srand((unsigned)time(NULL));
+
+    // creating array
+    for(int i=0; i<n; i++){
+        arr[i]= rand()%5;
+    }
+
+    // displaying array
+    for(int i= 0; i<n; i++){
+        cout<<arr[i]<<", ";
+    }
+
+    cout<<endl;
+    
+    // sorting array
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[i]){
+                int tmp= arr[j];
+                arr[j]= arr[i];
+                arr[i]= tmp;
+            }
+        }
+    }
+
+    // deleting duplicates
+    for(int i=0; i<n-1; i++){
+        if(arr[i]!=arr[i+1]){
+            arr[j]=arr[i];
+            cout<<arr[j]<<", ";
+            j++;
+        }
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Data Structure
 Data Structure using c++
+
+# Index
+
+01. WAP for removing duplicate values from an array.
